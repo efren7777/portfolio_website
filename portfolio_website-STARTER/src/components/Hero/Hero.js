3e8a4c1362a6d81e7dd3c801a4f7470ef3b89d63
Evan French <quantum.canvas7@gmail.com>
2023-05-27 18:35:19 -0400

    adding hero section

--- portfolio_website-STARTER/src/components/Hero/Hero.js
+++ portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        my portfolio
+      </SectionTitle>
+      <SectionText>
+        This is just a cool portfolio project to show off
+      </SectionText>
+      <Button onClick={() => window.location='https://google.com'}> Google</Button>
+
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
